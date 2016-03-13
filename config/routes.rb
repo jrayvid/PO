5dc3f95033a91d16ece5e2054063aeac619e1fcc
jon <jrayvid@gmail.com>
2016-03-13 16:39:41 -0400

    added comments CRUD for message boards

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-resources :messages
+resources :messages do
+  resources :comments
+end
 root 'messages#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
